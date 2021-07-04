e3e9273ad2f4f2fb690e45737683a6ab77a8162a
Alex <grinderbrader@gmail.com>
2021-07-04 20:05:54 +0600

    updates

--- src/animals.config.js
+++ src/animals.config.js
@@ -1,34 +1,34 @@
 export const config = {
     dog: {
-        bgImage: '/img/dog.jpg',
+        bgImage: 'https://cdn.mypup.io.s3.eu-north-1.amazonaws.com/dog.jpg',
         displayName: {
             en: 'your presious puppy',
             ru: 'своего щеночка'
         },
     },
     cat: {
-        bgImage: '/img/cat.jpg',
+        bgImage: 'https://cdn.mypup.io.s3.eu-north-1.amazonaws.com/cat.jpg',
         displayName: {
             en: 'your presious kitty',
             ru: 'своего котёнка'
         },
     },
     hamster: {
-        bgImage: '/img/hamster.jpg',
+        bgImage: 'https://cdn.mypup.io.s3.eu-north-1.amazonaws.com/hamster.jpg',
         displayName: {
             en: 'your presious hamster',
             ru: 'своего хомячка'
         },
     },
     horse: {
-        bgImage: '/img/horse.jpg',
+        bgImage: 'https://cdn.mypup.io.s3.eu-north-1.amazonaws.com/horse-progressive.jpeg',
         displayName: {
             en: 'your presious horsie',
             ru: 'своего жеребёнка'
         },
     },
     fish: {
-        bgImage: '/img/fish.jpg',
+        bgImage: 'https://cdn.mypup.io.s3.eu-north-1.amazonaws.com/fish.jpg',
         displayName: {
             en: 'your presious fish',
             ru: 'своей рыбки'
