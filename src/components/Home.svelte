b6d1d58f0cb013cd3eca18676b55c04bec692014
kngfhrr <kngfhrr@gmail.com>
2021-07-04 21:47:03 +0600

    animation

--- src/components/Home.svelte
+++ src/components/Home.svelte
@@ -1,16 +1,16 @@
 <script>
-  export let animal = 'dog';
-  export let language = 'en';
-  import { config } from '../animals.config';
-  import texts from '../texts';
+  export let animal = 'dog'
+  export let language = 'en'
+  import { config } from '../animals.config'
+  import texts from '../texts'
   import ColorChagin from './ColorChagin.svelte'
+  import Explosion from "./explosion.svelte"
   import { fly } from 'svelte/transition'
-  import { onMount } from 'svelte';
-  import { goto } from '@sapper/app';
-  let currentConfig = config[animal] ? config[animal] : config['dog'];
-  let currentText = texts[language] ? texts[language] : texts['en'];
-
-
+  import { onMount } from 'svelte'
+  import { goto } from '@sapper/app'
+  let currentConfig = config[animal] ? config[animal] : config['dog']
+  let currentText = texts[language] ? texts[language] : texts['en']
+  
   onMount(async () => {
     if (!texts[language]) {
       goto('/')
@@ -18,16 +18,16 @@
     if (!config[animal]) {
       goto('/')
     }
-  });
-  let value;
+  })
+  let value
   let visible = false
   let loading = false
   let genderList = {
     0: 'male',
-    1: 'female'
-  };
-  let selectedGender = 0;
-  let genders = ['Male', 'Female']  
+    1: 'female',
+  }
+  let selectedGender = 0
+  let genders = ['Male', 'Female']
   let category = 'dog'
   let currentName = ''
 
@@ -59,6 +59,12 @@
     visible = !visible
     currentName = ''
   }
+
+
+
+
+
+
 </script>
 
 <svelte:head>
@@ -68,16 +74,17 @@
 <section class="hero with-img is-light" style="background-image: url({currentConfig.bgImage})">
   <div class="hero-body pl-6 with-opacity">
     <div class="container">
-      <div>
-        <ColorChagin />
-      </div>
-      <h1 class="title is-size-1 has-text-light transition-text">{currentText.title[0]} <br/> {currentText.title[1]} {currentConfig.displayName[language]}</h1>
-
+      <h1 class="title is-size-1 has-text-light transition-text">
+        {currentText.title[0]} <br />
+        {currentText.title[1]}
+        {currentConfig.displayName[language]}
+      </h1>
+      
       <div class="wrap-input-generation">
         <div class="wrap-select">
           <select class="custom-select" bind:value>
             {#each currentText.genderSelect as gender}
-              <option value={currentText.genderSelect.findIndex((gen) => gen === gender)}>{gender}</option>
+              <option value={currentText.genderSelect.findIndex(gen => gen === gender)}>{gender}</option>
             {/each}
           </select>
           <div class="line" />
@@ -87,31 +94,37 @@
           <input class="input is-large input0-generation" type="text" placeholder={currentText.inputPlaceholder} />
         </div>
 
-        <button on:click={getName} style={{ zIndex: 0 }} class="button is-info is-large">{currentText.submitButton}</button>
+        <button on:click={getName} style={{ zIndex: 0 }} class="button is-info is-large"
+          >{currentText.submitButton}</button
+        >
       </div>
       <div class="mt-3">
-        <h2 class="has-text-light transition-text"
-          >{currentText.subtitle}
-      </h2>
+        <h2 class="has-text-light transition-text">{currentText.subtitle}</h2>
 
         {#if visible}
           <div transition:fly={{ duration: 400 }} class="generated-name-wrap">
             <div class="generated-name-msg area">
               {#if loading}
-              <div class="lds-ring"><div></div><div></div><div></div><div></div></div>
+                <div class="lds-ring">
+                  <div />
+                  <div />
+                  <div />
+                  <div />
+                </div>
               {:else}
-              <ul class="circles">
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-        </ul>
+                <ul class="circles">
+                  <li />
+                  <li />
+                  <li />
+                  <li />
+                  <li />
+                  <li />
+                  <li />
+                  <li />
+                  <li />
+                  <li />
+                </ul>
+              <Explosion />
                 <span>{currentText.nameModalTitle}</span>
                 <div class="generated-name">{currentName}</div>
                 <button on:click={closePopup} class="button is-success mt-5">{currentText.nameModalButton}</button>
@@ -126,38 +139,35 @@
 
 <style>
 
-
-.context {
+  .context {
     /* width: 100%; */
     /* position: absolute; */
-    top:50vh;
-    
-}
+    top: 50vh;
+  }
 
-.context h1{
+  .context h1 {
     text-align: center;
     color: #fff;
     font-size: 50px;
-}
+  }
 
-
-.area{
-    background: #4e54c8;  
-    background: -webkit-linear-gradient(to left, #8f94fb, #4e54c8);  
+  .area {
+    background: #4e54c8;
+    background: -webkit-linear-gradient(to left, #8f94fb, #4e54c8);
     /* width: 100%; */
-    height:100vh;
-}
+    height: 100vh;
+  }
 
-.circles{
+  .circles {
     position: absolute;
     bottom: 100px;
     left: 0;
     width: 100%;
     height: 100vh;
     overflow: hidden;
-}
+  }
 
-.circles li{
+  .circles li {
     position: absolute;
     display: block;
     list-style: none;
@@ -166,162 +176,147 @@
     background: rgba(255, 255, 255, 0.2);
     animation: animate 25s linear infinite;
     bottom: -150px;
-    
-}
+  }
 
-.circles li:nth-child(1){
+  .circles li:nth-child(1) {
     left: 25%;
     width: 80px;
     height: 80px;
     animation-delay: 0s;
-}
+  }
 
-
-.circles li:nth-child(2){
+  .circles li:nth-child(2) {
     left: 10%;
     width: 20px;
     height: 20px;
     animation-delay: 2s;
     animation-duration: 12s;
-}
+  }
 
-.circles li:nth-child(3){
+  .circles li:nth-child(3) {
     left: 70%;
     width: 20px;
     height: 20px;
     animation-delay: 4s;
-}
+  }
 
-.circles li:nth-child(4){
+  .circles li:nth-child(4) {
     left: 40%;
     width: 60px;
     height: 60px;
     animation-delay: 0s;
     animation-duration: 18s;
-}
+  }
 
-.circles li:nth-child(5){
+  .circles li:nth-child(5) {
     left: 65%;
     width: 20px;
     height: 20px;
     animation-delay: 0s;
-}
+  }
 
-.circles li:nth-child(6){
+  .circles li:nth-child(6) {
     left: 75%;
     width: 110px;
     height: 110px;
     animation-delay: 3s;
-}
+  }
 
-.circles li:nth-child(7){
+  .circles li:nth-child(7) {
     left: 35%;
     width: 150px;
     height: 150px;
     animation-delay: 7s;
-}
+  }
 
-.circles li:nth-child(8){
+  .circles li:nth-child(8) {
     left: 50%;
     width: 25px;
     height: 25px;
     animation-delay: 15s;
     animation-duration: 45s;
-}
+  }
 
-.circles li:nth-child(9){
+  .circles li:nth-child(9) {
     left: 20%;
     width: 15px;
     height: 15px;
     animation-delay: 2s;
     animation-duration: 35s;
-}
+  }
 
-.circles li:nth-child(10){
+  .circles li:nth-child(10) {
     left: 85%;
     width: 150px;
     height: 150px;
     animation-delay: 0s;
     animation-duration: 11s;
-}
+  }
 
-
-
-@keyframes animate {
-
-    0%{
-        transform: translateY(0) rotate(0deg);
-        opacity: 1;
-        border-radius: 0;
+  @keyframes animate {
+    0% {
+      transform: translateY(0) rotate(0deg);
+      opacity: 1;
+      border-radius: 0;
     }
 
-    100%{
-        transform: translateY(-2000px) rotate(720deg);
-        opacity: 0;
-        border-radius: 50%;
+    100% {
+      transform: translateY(-2000px) rotate(720deg);
+      opacity: 0;
+      border-radius: 50%;
     }
-
-}
-
-
-
-
-
-
-
-
-
-.transition-text {
-  animation: slide 1s;
-  position: relative;
-}
-@keyframes slide {
-  from {
-   opacity: 0;
-   left: -300px;
   }
-  to {
-    opacity: 1;
-    left: 0;
-  }
-}
 
-.lds-ring {
-  display: inline-block;
-  position: relative;
-  width: 80px;
-  height: 80px;
-}
-.lds-ring div {
-  box-sizing: border-box;
-  display: block;
-  position: absolute;
-  width: 64px;
-  height: 64px;
-  margin: 8px;
-  border: 8px solid #5FC578;
-  border-radius: 50%;
-  animation: lds-ring 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite;
-  border-color: #5FC578 transparent transparent transparent;
-}
-.lds-ring div:nth-child(1) {
-  animation-delay: -0.45s;
-}
-.lds-ring div:nth-child(2) {
-  animation-delay: -0.3s;
-}
-.lds-ring div:nth-child(3) {
-  animation-delay: -0.15s;
-}
-@keyframes lds-ring {
-  0% {
-    transform: rotate(0deg);
+  .transition-text {
+    animation: slide 1s;
+    position: relative;
   }
-  100% {
-    transform: rotate(360deg);
+  @keyframes slide {
+    from {
+      opacity: 0;
+      left: -300px;
+    }
+    to {
+      opacity: 1;
+      left: 0;
+    }
   }
-}
 
+  .lds-ring {
+    display: inline-block;
+    position: relative;
+    width: 80px;
+    height: 80px;
+  }
+  .lds-ring div {
+    box-sizing: border-box;
+    display: block;
+    position: absolute;
+    width: 64px;
+    height: 64px;
+    margin: 8px;
+    border: 8px solid #5fc578;
+    border-radius: 50%;
+    animation: lds-ring 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite;
+    border-color: #5fc578 transparent transparent transparent;
+  }
+  .lds-ring div:nth-child(1) {
+    animation-delay: -0.45s;
+  }
+  .lds-ring div:nth-child(2) {
+    animation-delay: -0.3s;
+  }
+  .lds-ring div:nth-child(3) {
+    animation-delay: -0.15s;
+  }
+  @keyframes lds-ring {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
 
   .with-img {
     background-repeat: no-repeat;
@@ -386,17 +381,17 @@
     animation: slide-input 1s;
   }
   @keyframes slide-input {
-  from {
-   opacity: 0;
-   width: 10%;
-   left: 30%;
+    from {
+      opacity: 0;
+      width: 10%;
+      left: 30%;
+    }
+    to {
+      opacity: 1;
+      left: 0;
+      width: 100%;
+    }
   }
-  to {
-    opacity: 1;
-    left: 0;
-    width: 100%;
-  }
-}
 
   @media screen and (max-width: 768px) {
     .wrap-input-generation {
---
+++ src/components/explosion.svelte
@@ -0,0 +1,25 @@
+
+<script>
+  import {onMount} from 'svelte'
+
+
+
+
+</script>
+
+<style>
+
+.party-popper {
+	position: absolute;
+	top: 50%;
+	left: 50%;
+}
+
+</style>
+
+<div class="party-popper">
+	<div class="party-popper__emoji"></div>
+</div>
+
+
+
